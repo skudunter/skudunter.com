ee0fac7d7e69dfe9e86a864098b41ddbc836e8c0
skudunter <danielgruttercpt@gmail.com>
2023-09-21 19:38:23 +0200

    feat added path

--- app/portfolio/[title]/page.tsx
+++ app/portfolio/[title]/page.tsx
@@ -1,18 +1,19 @@
 import { readFileSync, existsSync } from "fs";
 import { ReactNode } from "react";
 import { marked } from "marked";
+import path from 'path';
 
 export default function ProjectPage({ params }: { params: { title: string } }) {
   return <>{getMarkDownFileContent(params.title)}</>;
 }
 
 function getMarkDownFileContent(fileName: string): ReactNode {
-  const filePath: string = process.env.ISDEPLOYED == 'true'
-    ? `/content/${fileName}.md`
-    : `./public/content/${fileName}.md`;
- 
+  const isDeployed = process.env.ISDEPLOYED === 'true';
+  const basePath = isDeployed ? '/content' : './public/content';
+  const filePath = path.join(process.cwd(), basePath, `${fileName}.md`);
+
   if (existsSync(filePath)) {
-    const markdownContent: string = readFileSync(filePath, "utf8");
+    const markdownContent = readFileSync(filePath, 'utf8');
 
     return (
       <div dangerouslySetInnerHTML={{ __html: marked(markdownContent) }} />
@@ -25,3 +26,9 @@ function getMarkDownFileContent(fileName: string): ReactNode {
     );
   }
 }
+
+
+
+
+
+
